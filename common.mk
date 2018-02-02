a657361a8007ca686939c0d41525108818c15f78
Wei Wang <wvw@google.com>
2018-02-02 13:01:38 -0800

    sm6250-common: add SysUI to PRODUCT_DEXPREOPT_SPEED_APPS
    Test: Build
    Change-Id: I088938462b389e1ebe783997f773dc5946345666
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- common.mk
+++ common.mk
@@ -76,6 +76,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
