d7f84dad36a5b59c681356ab2edb014c52fd8220
Nick C <ctwoon@pixelexperience.org>
2020-08-10 22:02:21 +0300

    sm6250-common: lineage => pe

--- common.mk
+++ common.mk
@@ -144,13 +144,13 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-pe \
     $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
+    $(LOCAL_PATH)/overlay-pe/lineage-sdk \
+    $(LOCAL_PATH)/overlay-pe/packages/apps/Snap \
     $(LOCAL_PATH)/overlay-system
 
 # Partitions
