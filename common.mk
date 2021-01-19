d37912977dd18dac089780bd28e7d4a5263463cb
Sonal Singh <sonal.singh.19993@gmail.com>
2021-01-19 16:57:40 +0000

    sm6250-common: prebuilts: Add GoogleCameraGo

--- common.mk
+++ common.mk
@@ -105,7 +105,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    Snap
+    GoogleCameraGo
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
