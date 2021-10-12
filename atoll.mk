7f99605cccc45358dc815d51890c222a650cc9ce
Alexander Winkowski <dereference23@outlook.com>
2021-10-12 13:00:29 +0200

    sm6250-common: Update thermal HAL
     * From hardware/google/pixel at d774cbb949e98627e4172bf8fc11e8d954599aa7.
    
    Change-Id: I3a3a0c29575d0595e71a30f1e64e33ca34d2eb27

--- atoll.mk
+++ atoll.mk
@@ -386,7 +386,8 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.xiaomi_atoll
+    android.hardware.thermal@2.0-service.xiaomi_atoll \
+    thermal_symlinks
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
